f06ac11738cb533a81d7aa34f305a9a57c482369
Julian Michel <mail@julianmichel.de>
2026-01-17 22:42:25 +0100

    initial commit (cdk repo)

---
+++ test/github-oidc-aws-cdk-deploy.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as GithubOidcAwsCdkDeploy from '../lib/github-oidc-aws-cdk-deploy-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/github-oidc-aws-cdk-deploy-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new GithubOidcAwsCdkDeploy.GithubOidcAwsCdkDeployStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
